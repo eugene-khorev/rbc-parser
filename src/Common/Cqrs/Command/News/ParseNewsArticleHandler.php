1fad576cc58f7d52c1d330d67d23765e8bcc3738
Eugene Khorev <eugene.khorev@gmail.com>
2021-01-26 07:50:22 +0300

    Storing news articles in database

--- src/Common/Cqrs/Command/News/ParseNewsArticleHandler.php
+++ src/Common/Cqrs/Command/News/ParseNewsArticleHandler.php
@@ -7,6 +7,8 @@ namespace App\Common\Cqrs\Command\News;
 use App\Common\Cqrs\Command\CommandHandlerInterface;
 use App\Common\Parser\DataProviderInterface;
 use App\Common\Parser\Html\NewsArticleParserInterface;
+use App\Entity\NewsArticle;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -19,13 +21,15 @@ final class ParseNewsArticleHandler implements CommandHandlerInterface
     /**
      * ParseNewsArticleHandler constructor.
      * @param LoggerInterface $logger Logger service
+     * @param EntityManagerInterface $entityManager Doctrine entity manager
      * @param DataProviderInterface $dataProvider Data provider for parser
      * @param NewsArticleParserInterface $newsArticleParser News article parser service
      */
     public function __construct(
         private LoggerInterface $logger,
+        private EntityManagerInterface $entityManager,
         private DataProviderInterface $dataProvider,
-        private NewsArticleParserInterface $newsArticleParser
+        private NewsArticleParserInterface $newsArticleParser,
     ) {}
 
     /**
@@ -42,15 +46,63 @@ final class ParseNewsArticleHandler implements CommandHandlerInterface
 
             // Prepare parser and store news article data
             $this->newsArticleParser->prepare($content);
-            $this->logger->info('News article parsed', [
-                $articleLink,
-                $this->newsArticleParser->getArticleDate(),
-                $this->newsArticleParser->getArticleTile(),
-                $this->newsArticleParser->getArticleImage(),
-                $this->newsArticleParser->getArticleBody(),
-            ]);
+
+            // Store article
+            $article = $this->storeArticle(
+                $url = $articleLink,
+                $published_at = $this->newsArticleParser->getArticleDate(),
+                $title = $this->newsArticleParser->getArticleTile(),
+                $image_url = $this->newsArticleParser->getArticleImage(),
+                $body = $this->newsArticleParser->getArticleBody(),
+            );
+
+            $this->logger->info('News article parsed', [$article]            );
         } catch (\Throwable $e) {
             $this->logger->error($e->getMessage(), [$articleLink]);
         }
+    }
+
+    /**
+     * Store news article via Doctrine entity repository
+     * @param string $url Article URL
+     * @param string $published_at Article publication date
+     * @param string $title Article title
+     * @param string $image_url Article image URL
+     * @param string $body Article body
+     * @return NewsArticle Article entity
+     * @throws \Exception
+     */
+    private function storeArticle(
+        string $url,
+        string $published_at,
+        string $title,
+        string $image_url,
+        string $body,
+    ): NewsArticle
+    {
+        // Get news article entity repository
+        $repository = $this->entityManager->getRepository(NewsArticle::class);
+
+        // Check if the article already exists by its URL
+        $article = $repository->findOneBy(['url' => $url]);
+        if (empty($article)) {
+            // Create a new article
+            $article = new NewsArticle;
+        }
+
+        // Set article fields
+        $article->setUrl($url);
+        $article->setPublishedAt(
+            new \DateTime($published_at) // 2021-01-26T06:32:27+03:00
+        );
+        $article->setTitle($title);
+        $article->setImageUrl($image_url);
+        $article->setBody($body);
+
+        // Store article data
+        $this->entityManager->persist($article);
+        $this->entityManager->flush();
+
+        return $article;
     }
 }
